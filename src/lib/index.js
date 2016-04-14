d310af22fe9cab5a7b5cce99fd8ead3ffb3198ea
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-04-13 17:30:25 -0700

    init refactor

---
+++ src/lib/index.js
@@ -0,0 +1,7 @@
+import Griddle from './griddle.js';
+import * as DefaultModules from './defaultModules';
+import * as StyleHelpers from './utils/styleHelper';
+
+export { Griddle as Griddle }
+export { DefaultModules as DefaultModules }
+export { StyleHelpers as StyleHelpers }
